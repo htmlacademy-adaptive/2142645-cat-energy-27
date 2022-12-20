500269fda729fc968bc771946a307b0e1fa1943b
Владислав <vlados66625@mail.ru>
2022-12-20 19:18:25 +0300

    5 дз для наставника

---
+++ source/script.js
@@ -0,0 +1,14 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--open');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--open');
+  }
+});
